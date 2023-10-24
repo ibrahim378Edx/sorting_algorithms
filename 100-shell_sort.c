50c83fadd0ead22db73537cce8f1d89a16c00217
ibrahim378Edx <werdewer@yahoo.com>
2023-10-25 01:39:46 +0300

    task 4

---
+++ 100-shell_sort.c
@@ -0,0 +1,45 @@
+#include "sort.h"
+
+/**
+ * shell_sort - shell method
+ *
+ * @array: list to sort
+ *
+ * @size: size
+ *
+ * Return: nothing
+*/
+
+void shell_sort(int *array, size_t size)
+{
+int in = 0, out = 0, r = 1, a = 0;
+
+if (!array || size < 2)
+{
+	return;
+}
+while (r < (int) size / 3)
+{		
+r = r * 3 + 1;
+}
+while (r > 0)
+{
+out = r;
+while (out < (int) size)
+{
+a = array[out];
+in = out;
+
+while (in > r - 1 && array[in - r] >= a)
+{
+array[in] = array[in - r];
+in = in - r;
+}
+
+array[in] = a;
+out++;
+}
+print_array(array, size);
+r = (r - 1) / 3;
+}
+}
